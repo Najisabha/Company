325dac5c1f017d4a7416548e622bb204fe20be01
naji sabha <nageammar628@gmail.com>
2025-03-10 07:45:05 +0200

    V1.0

--- src/js/index.js
+++ src/js/index.js
@@ -3,13 +3,13 @@ import '../sass/style.scss';
 // import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
-import 'jquery/dist/jquery.min';
+import 'jquery/dist/jquery.js';
 import 'popper.js/dist/popper.min';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import '@fortawesome/fontawesome-free/js/all.min.js';
 
 $(function(){
-    $('.thumbnail2').hover(function(){
+    $('.thumbnali2').hover(function(){
         $(this).find('.project-category').hide();
         $(this).find('.caption2').slideDown(250);
     }
